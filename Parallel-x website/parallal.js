61e52a074bfc53957b650591c3c33a83d9fcd4a7
Dhairya Gothi <dhairyag31@gmail.com>
2024-02-29 00:27:25 +0530

    deployment

--- Parallel-x website/parallal.js
+++ Parallel-x website/parallal.js
@@ -3,14 +3,16 @@ let  leaf =document.getElementById("leaf")
 let  hill5 =document.getElementById("hill5")
 let  hill1 =document.getElementById("hill1")
 let  plant =document.getElementById("plant")
+let hill4 =document.getElementById("hill4")
 
 window.addEventListener('scroll',()=>{
     let value = window.scrollY;
 
     text1.style.marginTop = value * 2.5 +'px';
     leaf.style.left= value * 2 +'px';
-    hill1.style.top= value * -0.25 +'px';
+    hill1.style.top= value * 0.25 +'px';
     hill5.style.left= value * 1 +'px';
-
+    hill4.style.left=value *-0.75+'px';
+    plant.style.marginTop = value * 0.5 +'px';
 
 })
