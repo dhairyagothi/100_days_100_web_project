7d5f2533e89299944c0ac7106c08348608d1deef
Dhairya Gothi <dhairyag31@gmail.com>
2024-04-18 18:12:57 +0530

    day20

--- public/Parallel-x website/parallal.js
+++ public/Parallel-x website/parallal.js
@@ -8,7 +8,7 @@ let hill4 =document.getElementById("hill4")
 window.addEventListener('scroll',()=>{
     let value = window.scrollY;
 
-    text1.style.marginTop = value * 2.5 +'px';
+    text1.style.marginTop = value * 1.5 +'px';
     leaf.style.left= value * 2 +'px';
     hill1.style.top= value * 0.25 +'px';
     hill5.style.left= value * 1 +'px';
