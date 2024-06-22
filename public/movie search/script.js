d09747dfa3a34f4257cab71a4f2f231348577e9a
Monish.R <140420819+MonishRaman@users.noreply.github.com>
2024-06-22 17:52:51 +0530

    updated

--- public/movie search/script.js
+++ public/movie search/script.js
@@ -1,4 +1,4 @@
-const API_KEY = "api_key=1cf50e6248dc270629e802686245c2c8";
+const API_KEY = "api_key=91f41d3ef950816ce6f2445e7a81ddae";
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
