e21e08ae9d4e4995793e395d3ffb2e2aea4847a3
externref <shiva02939@gmail.com>
2024-06-24 07:59:56 +0530

    add pastebin

---
+++ public/pastebin/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				branding_dark: '#121213',
+				branding_orange: '#EEA867'
+			}
+		}
+	},
+	plugins: []
+};
