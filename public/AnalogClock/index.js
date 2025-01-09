497410ac87521962ceda27d4c82d6db3ce3956cd
suhasiniChoudhari <suhasinichoudhari1@gmail.com>
2025-01-09 18:20:41 +0530

    Added timer feature to analog clock and improved UI

--- public/AnalogClock/index.js
+++ public/AnalogClock/index.js
@@ -3,11 +3,49 @@ setInterval(() => {
     htime = d.getHours();
     mtime = d.getMinutes();
     stime = d.getSeconds();
-    hrotation = 30*htime + mtime/2;
-    mrotation = 6*mtime;
-    srotation = 6*stime;
+    hrotation = 30 * htime + mtime / 2;
+    mrotation = 6 * mtime;
+    srotation = 6 * stime;
 
     hour.style.transform = `rotate(${hrotation}deg)`;
     minute.style.transform = `rotate(${mrotation}deg)`;
     second.style.transform = `rotate(${srotation}deg)`;
 }, 1000);
+
+// Countdown Timer
+let countdownInterval;
+let countdownTime;
+
+function startCountdown() {
+    let hours = document.getElementById('hours').value || 0;
+    let minutes = document.getElementById('minutes').value || 0;
+    let seconds = document.getElementById('seconds').value || 0;
+
+    countdownTime = (hours * 3600) + (minutes * 60) + seconds;
+
+    clearInterval(countdownInterval); // Clear any previous countdowns
+
+    countdownInterval = setInterval(() => {
+        if (countdownTime <= 0) {
+            clearInterval(countdownInterval);
+
+            //show pop up msg
+
+            document.getElementById('timerUpMsg').style.display = 'block';
+
+
+            // Play alarm sound
+            const timerSound = document.getElementById('timerSound');
+            timerSound.play();
+
+
+        } else {
+            countdownTime--;
+            let hoursLeft = Math.floor(countdownTime / 3600);
+            let minutesLeft = Math.floor((countdownTime % 3600) / 60);
+            let secondsLeft = countdownTime % 60;
+            document.getElementById('countdownDisplay').textContent =
+                `${String(hoursLeft).padStart(2, '0')}:${String(minutesLeft).padStart(2, '0')}:${String(secondsLeft).padStart(2, '0')}`;
+        }
+    }, 1000);
+}
