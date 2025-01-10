151a313f17cecb3729d05d270e8143d4964d6d2c
Rohan Nayan <rohannayan405@gmail.com>
2025-01-10 21:44:22 +0530

    Minor Changes

--- public/text-to-readme/src/App.js
+++ public/text-to-readme/src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
   return (
     <div className="App">
+      <h1 className='app-h1'>Text 2 Readme!</h1>
     </div>
   );
-}
+};
 
 export default App;
--- public/text-to-readme/src/index.js
+++ public/text-to-readme/src/index.js
@@ -8,3 +8,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
