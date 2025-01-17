5181166abf157aa5d62b3b42f0efb0cb13115b17
Khushi-bhaskar01 <khushibhaskar.2006@gmail.com>
2025-01-17 13:50:21 +0530

    RECIPE_Genie
    This is my first project using Next.js, showcasing my journey into full-stack development. It serves as a foundation for building innovative solutions in the kitchen.Framework: Next.js
    Styling: Tailwind CSS

---
+++ public/RECIPE_Genie/my-project/src/app/layout.tsx
@@ -0,0 +1,29 @@
+"use client"
+import "./globals.css"
+import Navbar from "../../components/Navbar";
+import { ThemeProvider } from "next-themes"
+import { hydrateRoot } from "react-dom/client";
+import { Footer } from "../../components/Footer";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        <ThemeProvider enableSystem={true} attribute="class">
+          <Navbar />
+          {children}
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
