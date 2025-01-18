7211d7e7e4e1d01e34ac3bc09cd478f6a9d0fa87
Harsh Srivastava <harshme08@gmail.com>
2025-01-18 17:32:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -165,7 +165,10 @@ function fillTable() {
         ["Day 94","Hangman Game","./public/HangmanGame/index.html"],
         ["Day 95","TodoList in React TS Tailwind","./public/TodoList-React-TS-Tailwind/index.html"],
       ["Day 96","HCL Color Generator","./public/HCL Color Generator/index.html"],
-      ["Day 97","Time Capsule","public/Time-Capsule/index.html"]
+      ["Day 97","Time Capsule","public/Time-Capsule/index.html"],
+        ["Day 98","Drumkit Game","public/Drumkit_Game/index.html"],
+        
+        
 
 
     ];
