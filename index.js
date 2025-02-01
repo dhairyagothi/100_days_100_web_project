dd9f92103c0528602f0915761bbe66b4c218dbde
dfordebarati <sealdebarati3@gmail.com>
2025-02-01 19:23:31 +0530

    Added the website

--- index.js
+++ index.js
@@ -179,7 +179,8 @@ function fillTable() {
         ["Day 107", "expense_Tracker", "./public/expense_Tracker/index.html"],
       ["Day 108","Mood Tracker","./public/Mood Tracker/index.html"],
     ["Day 109","CRYPTOSHOW","/public/CRYPTOSHOW/index.html"],
-      ["Day 110","Whack-a-Mole Game","./public/Whack-a-Mole Game/index.html"]
+      ["Day 110","Whack-a-Mole Game","./public/Whack-a-Mole Game/index.html"],
+      ["Day 111","Nykaa Clone Website","./public/Nykaa-clone/index.html"]
     ];
 
 
