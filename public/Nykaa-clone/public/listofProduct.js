009ee783619edc09e01ab7e34a861b60cde21e46
dfordebarati <sealdebarati3@gmail.com>
2025-02-01 19:17:31 +0530

    Added all the files

---
+++ public/Nykaa-clone/public/listofProduct.js
@@ -0,0 +1,51 @@
+export default [
+    {
+        img : "https://images-static.nykaa.com/uploads/beb9fdeb-e96c-4c55-9138-ba487e5800aa.jpg?tr=w-200,cm-pad_resize",
+        dis : "Up to 60% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/228bcd54-9099-4998-b4c3-777f77e592f6.jpg?tr=w-200,cm-pad_resize",
+        dis : "Up to 50% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/5c50557a-cf1e-4500-8b0f-4b9321fce87e.jpg?tr=w-200,cm-pad_resize",
+        dis : "Up to 70% off"
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/6529686c-e1f9-4c33-98e8-d146493424db.jpg?tr=w-200,cm-pad_resize",
+        dis : "Up to 50% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/6529686c-e1f9-4c33-98e8-d146493424db.jpg?tr=w-200,cm-pad_resize",
+        dis : "Up to 70% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/9bf0f694-34b5-4960-9f0a-a6069445532d.jpg?tr=w-200,cm-pad_resize",
+        dis : "Up to 70% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/1ee17084-9905-4921-ab01-9fb174d727ee.jpg?tr=w-200,cm-pad_resize",
+        dis : "Up to 50% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/1ee17084-9905-4921-ab01-9fb174d727ee.jpg?tr=w-200,cm-pad_resize",
+        dis : "Exciting offers",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/bc749a66-166c-464d-b1c6-71a64d5c64e1.jpg?tr=w-200,cm-pad_resize",
+        dis : "Up to 50% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/fbc882ec-ed72-4d57-845b-4811136c85de.jpg?tr=w-200,cm-pad_resize",
+        dis : "Up to 40% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/5474f425-87ad-4ba4-bede-3bdf693f64a3.jpg?tr=w-200,cm-pad_resize",
+        dis : "Up to 40% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/f33c088a-b73d-4c13-9741-874e91e6a466.jpg?tr=w-200,cm-pad_resize",
+        dis : "min 10% off",
+    }
+]
+    
---
+++ public/Nykaa-clone/topbrands.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        img : "https://images-static.nykaa.com/uploads/6e350752-7c60-431b-9474-add3b33395fa.jpg?tr=w-240,cm-pad_resize",
+        dis : "Up to 65% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/b3b7dd8d-d8ef-450b-9213-d438db1c9fcc.jpg?tr=w-240,cm-pad_resize",
+        dis : "Up to 50% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/2a249456-6efe-47d8-a1bb-9165ffca1f84.jpg?tr=w-240,cm-pad_resize",
+        dis : "Up to 50% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/ce00fd6a-093b-4040-bfac-e587b8d140cd.jpg?tr=w-240,cm-pad_resize",
+        dis : "Up to 80% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/3f1d8ebe-2829-439e-8f86-9db9ba4892bb.jpg?tr=w-240,cm-pad_resize",
+        dis : "Up to 65% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/00c6e704-18a2-4bcf-8712-83d6244fba05.jpg?tr=w-240,cm-pad_resize",
+        dis : "Up to 60% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/bb3760af-d016-45a4-b292-53f200a1232e.jpg?tr=w-240,cm-pad_resize",
+        dis : "Up to 60% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/d5759d1c-294c-49ac-a668-90cf21592de4.jpg?tr=w-240,cm-pad_resize",
+        dis : "Up to 50% off",
+    },
+    {
+        img : "https://images-static.nykaa.com/uploads/1acb3b6c-b4d6-43ee-bd75-39d4e85a1365.jpg?tr=w-240,cm-pad_resize",
+        dis : "Up to 70% off",
+    },
+]
