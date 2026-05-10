6ad290fedb26845f69143bb8830a8834b8ba4f20
Sahithi <sahithireddy422@gmail.com>
2026-05-10 13:17:38 +0000

    feat: add new project - Fireworks Click Effect

--- index.js
+++ index.js
@@ -283,7 +283,8 @@ function fillTable() {
         ["Day 111", "Whack-a-Mole Game", "./public/Whack-a-Mole Game/index.html"],
         ["Day 112", "Nykaa Clone Website", "./public/Nykaa-clone/index.html"],
         ["Day 113", "CPU Scheduler", "./public/CpuScheduler/index.html"],
-     ["Day 114","EchoNotes","./public/EchoNotes/index.html"]
+     ["Day 114","EchoNotes","./public/EchoNotes/index.html"],
+	["Day 115", "Fireworks Click Effect", "./public/Fireworks-Click-Effect/index.html"]
     ];
 
     const tbody = document.getElementById('tableBody');
