68c9a799bb043f9799dfbcc539c7d0f4172175f8
Shouri Chakraborty <107758587+shouri123@users.noreply.github.com>
2026-05-15 14:53:52 +0530

    fix: disable preloader to troubleshoot rendering issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <AuthProvider>
-            <Preloader />
+            {/* <Preloader /> */}
             <Navbar />
             <main className="flex-1">
               {children}
