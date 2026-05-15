c34ca8367319edfdb2bfd4b7de4450071a06ec6c
Sham Ganeshan SB <shamganeshansb@gmail.com>
2026-05-15 15:38:55 +0530

    fix: remove duplicate .then() in getWeather function

--- public/Weather Forcasting/script.js
+++ public/Weather Forcasting/script.js
@@ -1,39 +1,39 @@
-
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '2c06e2f780msh3f81c0245629ba6p19a960jsn5eff9cb95a57',
-        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-    }
-};
 const getWeather = (city) => {
-    cityName.innerHTML = city
+    cityName.innerHTML = city;
     fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
         .then(response => response.json())
         .then((response) => {
-            console.log(response)
+            console.log(response);
 
-            // cloud_pct.innerHTML = response.cloud_pct
-            temp.innerHTML = response.temp
-            temp2.innerHTML = response.temp
-            feels_like.innerHTML = response.feels_like
-            humidity.innerHTML = response.humidity
-            humidity2.innerHTML = response.humidity
-            min_temp.innerHTML = response.min_temp
-            max_temp.innerHTML = response.max_temp
-            wind_speed.innerHTML = response.wind_speed
-            wind_speed2.innerHTML = response.wind_speed
-            wind_degrees.innerHTML = response.wind_degrees
-            sunrise.innerHTML = response.sunrise
-            sunset.innerHTML = response.sunset
+            temp.innerHTML = response.temp;
+            temp2.innerHTML = response.temp;
+            feels_like.innerHTML = response.feels_like;
+            humidity.innerHTML = response.humidity;
+            humidity2.innerHTML = response.humidity;
+            min_temp.innerHTML = response.min_temp;
+            max_temp.innerHTML = response.max_temp;
+            wind_speed.innerHTML = response.wind_speed;
+            wind_speed2.innerHTML = response.wind_speed;
+            wind_degrees.innerHTML = response.wind_degrees;
+            sunrise.innerHTML = response.sunrise;
+            sunset.innerHTML = response.sunset;
 
-        })
+            if (response.cloud_pct > 70) {
+                document.body.style.background = "linear-gradient(to right, #757f9a, #d7dde8)";
+            } else if (response.humidity > 80) {
+                document.body.style.background = "linear-gradient(to right, #4b79a1, #283e51)";
+            } else if (response.temp > 30) {
+                document.body.style.background = "linear-gradient(to right, #f7971e, #ffd200)";
+            } else if (response.temp < 10) {
+                document.body.style.background = "linear-gradient(to right, #83a4d4, #b6fbff)";
+            } else {
+                document.body.style.background = "linear-gradient(to right, #4facfe, #00f2fe)";
+            }
+        })  // 👈 closes the .then()
         .catch(err => console.error(err));
-
 }
 
 submit.addEventListener("click", (e) => {
-    e.preventDefault()
-    getWeather(city.value)
-})
-
+    e.preventDefault();
+    getWeather(city.value);
+});
