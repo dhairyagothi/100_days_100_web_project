f639a1e0fc7b30aa79026d4bc3762c4f902df425
Anushka <anushkajagtap2212@gmail.com>
2026-05-15 22:49:59 +0530

    Added AetherCode

---
+++ public/AetherCode/script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
