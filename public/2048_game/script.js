4e31d6706a54cb51c7b6ea7440a2b5bd34bc2b9e
danishnehru <danishnehru22@gmail.com>
2026-05-16 10:09:23 +0530

    fix:resolve inverted left and right movement logic

--- public/2048_game/script.js
+++ public/2048_game/script.js
@@ -193,7 +193,7 @@ function doMove (dir) {
   prevBoard = copyBoard(board);
   prevScore = score;
 
-  const rotTurns = { right: 0, down: 1, left: 2, up: 3 };
+  const rotTurns = { right: 2, down: 1, left: 0, up: 3 };
   let tmp = rotateBoard(board, rotTurns[dir]);
   let pts = 0;
   let moved = false;
