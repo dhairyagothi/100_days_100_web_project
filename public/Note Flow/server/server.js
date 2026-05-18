0325f9ed1e33447dd1623055a72daeff8923514c
Anurag-3112 <ak7546824@gmail.com>
2026-05-19 05:27:36 +0530

    Create server.js

---
+++ public/Note Flow/server/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
