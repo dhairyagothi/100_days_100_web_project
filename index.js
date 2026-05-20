313e690c6631c9463177f9fdd219ca8e38c1521f
HajArshia <amirarshiaghaemi@gmail.com>
2026-05-20 14:04:25 +0330

    Add coffee-menu to PROJECT_DATA index.js

--- index.js
+++ index.js
@@ -135,6 +135,7 @@ const PROJECT_DATA = [
   ['Day 117', 'Habit Tracker Web App', './public/Habit-Tracker-Web-App/index.html', 'html css js', 'intermediate'],
   ['Day 118', 'Particle Effect', './public/particle-effect/index.html', 'html css js canvas', 'intermediate'],
   ['Day 119', 'Virtual Playground', './playground.html', 'html css js', 'intermediate'],
+  ['Day 120', 'Coffee Menu', './public/coffee-menu/public/start.html', 'html tailwindcss css js', 'intermediate'],
 ];
 
 // Alias for consistency
---
+++ public/coffee-menu/README.md
@@ -0,0 +1,22 @@
+# Sefr Coffee
+
+A simple front-end coffee menu project built with:
+
+- HTML
+- CSS
+- Tailwind CSS
+- JavaScript
+
+## Description
+This project is a minimal coffee product showcase.  
+It includes:
+
+- A splash/start screen
+- Category filters (e.g. all, ice-coffee, hot-coffee, americano, espresso)
+- Product cards with image, name, and price
+- Simple pagination
+
+## GitHub
+https://github.com/arshia-ghaemi
+
+
