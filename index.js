d5bc1d3b85c66f7c9ac238669c983f7cbc13651c
Ashish Kumar <rashishkumar2006@gmail.com>
2026-05-21 16:34:47 +0530

    Reorder project entries in index.js

--- index.js
+++ index.js
@@ -173,11 +173,11 @@ const PROJECT_DATA = [
    ['Day 135', 'Shooting game', './public/shooting game/index.html', '2d game', 'intermediate'],
    ['Day 136', 'Sudoku Solver', './public/sudoku-solver/index.html', 'game javascript', 'intermediate'],
   ['Day 137', 'Maths Quiz Game', './public/maths-quiz-game/index.html', 'game javascript', 'intermediate'],
-  ['Day 140', 'Age Calculator', './public/age-calculator/index.html', 'tool javascript', 'beginner'],
-  ['Day 141', 'Ludo game', './public/Ludo-game/index.html', 'Html css javascript', 'intermediate'],
-['Day 142', 'Big Sales Prediction',
+  ['Day 138', 'Age Calculator', './public/age-calculator/index.html', 'tool javascript', 'beginner'],
+  ['Day 139', 'Ludo game', './public/Ludo-game/index.html', 'Html css javascript', 'intermediate'],
+['Day 140', 'Big Sales Prediction',
  'https://github.com/dhairyagothi/100_days_100_web_project/tree/Main/public/BigSales-Prediction', 'machine learning python javascript', 'advanced'],
-
+  ['Day 141', 'Chess', './public/Chess/index.html', 'Strategy game', 'Advanced'], 
 ];
 // Alias for consistency
 const PROJECTS = PROJECT_DATA;
