42b083b977776329e71bacc6a6164859d05926a5
Rakhi <rakhipanwar819@gmail.com>
2026-05-23 00:31:43 +0530

    Added new project-ReadMeCraft-GitHub profile README generator

--- index.js
+++ index.js
@@ -229,6 +229,7 @@ const PROJECT_DATA = [
   ['Day 152', 'Physics Ball Simulation', './public/PhysicsBallSimulation/index.html', 'html css javascript canvas', 'advanced'],
   ['Day 153', 'Material3 Showcase', './public/Material3Showcase/index.html', 'tool javascript', 'intermediate'],
   ['Day 154', 'FocusRoom', './public/FocusRoom/index.html', 'html css javascript productivity timer tasks ambient', 'intermediate'],
+  ['Day 155', 'ReadMeCraft-GitHub profile README generator', './public/ReadMeCraft/index.html', 'html css javascript', 'intermediate'],
 ];
 const PROJECTS = PROJECT_DATA;
 
