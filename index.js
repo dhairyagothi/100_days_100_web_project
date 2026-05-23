191806c44df4d5750435e8eca205d1ee4cecabd6
Vaishali Varma <vaishalivarma200@gmail.com>
2026-05-23 14:13:18 +0530

    Add Movie Discovery Platform project to index

--- index.js
+++ index.js
@@ -228,6 +228,7 @@ const PROJECT_DATA = [
   ['Day 157', 'Map Route Tracker', './public/Vector-Map-Route-Tracer/index.html', 'html css javascript', 'advanced'],
   ['Day 158', 'GitHub Promo Maker', './public/GitHubPromoMaker/index.html', 'html css javascript', 'intermediate'],
   ['Day 159' , 'Dining Philosophers Simulation' , './public/Dining Philosophers Simulation/index.html' , 'simulation algorithm javascript' , 'intermediate' ] ,
+  ['Day 160', 'Movie Discovery Platform', './public/Movie_discovery_platform/index.html', 'api javascript movie ui', 'intermediate'],
 ];
 const PROJECTS = PROJECT_DATA;
 
