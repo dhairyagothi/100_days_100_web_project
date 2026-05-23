0a90acfd53274c7cb159fb3637ed5349aa79e410
NRZx07 <nirajjha655@gmail.com>
2026-05-23 21:22:30 +0545

    feat: add IP Address Tracker project

--- index.js
+++ index.js
@@ -129,7 +129,8 @@ const PROJECTS = [
     ["Day 113", "CPU Scheduler",                       "./public/CpuScheduler/index.html",                                                                               ["HTML","CSS","JS"],       "tool"],
     ["Day 114", "EchoNotes",                           "./public/EchoNotes/index.html",                                                                                  ["JS","SpeechAPI"],        "tool"],
     ["Day 115", "Event Registration System",           "https://event-registration-system-w10a.onrender.com/",                                                           ["Node","Express"],        "tool"],
-    ["Day 116", "AI Image Classifier",                 "./public/AI Image Classifier/index.html",                                                                        ["HTML","CSS","JS","AI"], "tool"],
+    ["Day 116", "AI Image Classifier",                 "./public/AI Image Classifier/index.html",                                                               ["JS","TensorFlow.js"],     "api"],  
+    ["Day 117", "Ip_Address_Tracker",                     "./public/IP_Address_Tracker/index.html",                                                                          ["JS","IP Geolocation API"], "Tool"],                                                               
 ];
 
 const CATEGORY_LABEL = {
