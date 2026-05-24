50a054a8ccfad9b0558192a709f4bb435563fbe2
SiddharthRiot <siddharthk9654@gmail.com>
2026-05-24 23:28:17 +0530

    fix: remove duplicate entries from merge conflict

--- index.js
+++ index.js
@@ -236,9 +236,7 @@ const PROJECT_DATA = [
   [ 'Day 165' , 'Amazon Clone' , './public/AmazonClone/index.html' , 'Amazon Clone HTML CSS JavaScript' , 'beginner' ],
   [ "Day 166" , "Boredom Buster" , "./public/BoredomBuster/index.html" , "html css javascript" , 'advanced' ],
   [ "Day 167" , "Color Sort Puzzle game" , "./public/colorsort/index.html" , "html css javascript" , 'advanced' ],
-  ['Day 168', 'Invoice / Bill Generator', './public/Day156_Invoice_Generator/index.html', 'tool javascript', 'intermediate']
-  ["Day 167", "scam-sms-detector", "/public/scam-sms-detector/index.html"],
-   [ "Day 168" , "Color Sort Puzzle game" , "./public/colorsort/index.html" , "html css javascript" , 'advanced' ]
+  ['Day 168', 'Invoice / Bill Generator', './public/Day156_Invoice_Generator/index.html', 'tool javascript', 'intermediate']git
 ];
 const PROJECTS = PROJECT_DATA;
 
