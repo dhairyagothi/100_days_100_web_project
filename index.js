7a91ea4e5ef138e09564a0f7a0c55bd8fa5eb53f
Gowtham sree charan reddy <iamgowthamsree@gmail.com>
2026-05-25 00:33:25 +0530

    fix: added missing tags and difficulty for Day 167 project to prevent page rendering crash

--- index.js
+++ index.js
@@ -235,7 +235,7 @@ const PROJECT_DATA = [
   [ 'Day 164' , 'Code Visualizer Playground' , './public/code-visualizer-playground/index.html' , 'tool javascript html css' , 'advanced' ],
   [ 'Day 165' , 'Amazon Clone' , './public/AmazonClone/index.html' , 'Amazon Clone HTML CSS JavaScript' , 'beginner' ],
   [ "Day 166" , "Boredom Buster" , "./public/BoredomBuster/index.html" , "html css javascript" , 'advanced' ],
-  ["Day 167", "scam-sms-detector", "/public/scam-sms-detector/index.html"],
+  ["Day 167", "scam-sms-detector", "./public/scam-sms-detector/index.html", "html css javascript tool", "intermediate"],
    [ "Day 168" , "Color Sort Puzzle game" , "./public/colorsort/index.html" , "html css javascript" , 'advanced' ]
 ];
 const PROJECTS = PROJECT_DATA;
