cf59294285e3112d57174544bda8ecd775748440
Goutam-2702 <kumargoutam2006@gmail.com>
2026-05-30 19:22:15 +0530

    fix: remove duplicate scroll to top button

--- index.js
+++ index.js
@@ -398,19 +398,4 @@ document.addEventListener('DOMContentLoaded', () => {
     initScrollBtn();
 });
 
-const backToTopButton = document.getElementById("backToTop");
 
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 200) {
-        backToTopButton.style.display = "block";
-    } else {
-        backToTopButton.style.display = "none";
-    }
-});
-
-backToTopButton.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
