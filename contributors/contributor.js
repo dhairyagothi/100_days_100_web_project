6b00ce8445a0c31b4a5257c522a9cb6cf5ba28b9
basantnema31 <pianistbasant31@gmail.com>
2026-06-03 11:10:42 +0530

    Fix: Optimize Application Load Time by Implementing Lazy Loading (#5026)

--- contributors/contributor.js
+++ contributors/contributor.js
@@ -184,6 +184,7 @@ async function openProfile(username) {
     const avatar = document.createElement('img');
     avatar.src = user.avatar_url;
     avatar.alt = user.name || username;
+    avatar.loading = 'lazy';
     avatar.style.width = '120px';
     avatar.style.height = '120px';
     avatar.style.borderRadius = '50%';
@@ -364,6 +365,7 @@ function renderContributors(data) {
     const avatar = document.createElement('img');
     avatar.src = contributor.avatar_url;
     avatar.alt = contributor.login;
+    avatar.loading = 'lazy';
     card.appendChild(avatar);
 
     // Name
@@ -448,6 +450,7 @@ function renderStargazers(stargazers) {
     const img = document.createElement('img');
     img.src = stargazer.avatar_url;
     img.alt = stargazer.login;
+    img.loading = 'lazy';
 
     starItem.appendChild(img);
 
