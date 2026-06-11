772af2b54feb97622bc95f84f25ee39f83f49531
Saurabh Kumar Bajpai <saurabhhhcodes@users.noreply.github.com>
2026-06-12 03:42:44 +0530

    feat: add connect4 to project index

--- index.js
+++ index.js
@@ -144,7 +144,8 @@ const PROJECT_DATA = [
   ['Day 125', 'Snakeladder',   './public/Snakeladder/index.html', 'game', 'intermediate'],
   ['Day 126', 'Temperature Converter', './public/TemperatureConverter/index.html', 'tool javascript', 'beginner'],
   ['Day 127', 'Particle Wave Animation', './public/Particle Wave Animation/index.html', 'css javascript', 'intermediate'],
-  ['Day 128', 'Reaction Time Test', './public/reaction-time-tester/main.html', 'animation simulation html css js javascript', 'intermediate']
+  ['Day 128', 'Reaction Time Test', './public/reaction-time-tester/main.html', 'animation simulation html css js javascript', 'intermediate'],
+  ['Day 129', 'Connect 4', './public/Connect4/index.html', 'game react', 'intermediate']
 ];
 
 // Alias for consistency
